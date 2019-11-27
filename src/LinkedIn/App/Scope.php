165788a26cb490b1f3c4245986658abbb2298b9a
Shailesh Daund <shailesh.daund@gmail.com>
2019-11-27 18:36:04 +0530

    App scope added

---
+++ src/LinkedIn/App/Scope.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the linkedIn PHP library.
+ *
+ * (c) Shailesh Daund <shailesh.daund@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.md.
+ *
+ * Source: https://github.com/shailesh-daund/linkedIn-php
+ */
+
+namespace LinkedIn\App;
+/**
+ * The possible application scopes.
+ *
+ * @author Shailesh Daund <shailesh.daund@gmail.com>
+ */
+class Scope
+{
+    const BASIC_PROFILE = 'r_basicprofile';
+    const LITE_PROFILE = 'r_liteprofile';
+    const EMAIL_ADDRESS = 'r_emailaddress';
+}
